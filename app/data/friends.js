5ec4544a6ae4f46d31199cc247251a8d2462c580
jlramsey <jj@juniors-MacBook-Pro.local>
2019-09-14 08:47:32 -0400

    update files

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Charles Cameron",
+		photo: "https://www.biography.com/.image/ar_8:10%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_620/MTIwNjA4NjM0MDQ0NDUwMzE2/medgar-evers-9542324-1-402.jpg",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Itchy Johnson",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvsDU4qcGs0WjtVhX6NkS4jeduntVbq6_Ra-w-b2sc_yFkz5hh",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "The Goat",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2r_ta2Z0w_mu2KM7UTsgvQLTZmGT5K0vh26kN7-R5nrlz2tF8-g",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
