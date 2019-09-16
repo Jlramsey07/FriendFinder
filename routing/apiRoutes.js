98dcdd035f30cca01cb65835613333426f42a018
jlramsey <jj@juniors-MacBook-Pro.local>
2019-09-16 18:28:49 -0400

    Deployment ready and fixes

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,7 +2,7 @@
 // The app.post(in the apiRoutes.js) used to handle the compatibility logic.
 
 
-var friends = require('../data/friends');
+var friends = require('../app/data/friends');
 
 // Routing the apiRoutes with the app.get and app.post functions
 module.exports = function (app) {
